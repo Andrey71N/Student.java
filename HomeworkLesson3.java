6327a1bbf8851ca261b264811811958683a9163e
nikolaitch <nikolaitch71@gmail.com>
2022-10-04 17:01:47 +0300

    Goo

---
+++ HomeworkLesson3.java
@@ -0,0 +1,29 @@
+package lesson3;
+
+public class HomeworkLesson3 {
+
+
+    public static void main(String[] args) {
+
+       System.out.println("Hellow cat");
+    }
+}
+
+
+      class HomeworkLesson4 {
+    public static void main(String[] args) {
+        System.out.println("Sky");
+    }
+}
+
+
+
+    
+
+
+
+
+
+
+
+
